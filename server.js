505f77cd272af0a894dfdb206e37ea7785597486
briabr <filalibria@yahoo.fr>
2022-11-10 20:23:46 -0500

    port is dynamic

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
-const port = 3000
+//make the port variable dynamic
+const port = process.env || 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
