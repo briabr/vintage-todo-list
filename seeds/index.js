99e9bd178be3d9cb6dffd074e17c925fa667be63
briabr <filalibria@yahoo.fr>
2022-11-10 23:07:45 -0500

    adds User model

--- seeds/index.js
+++ seeds/index.js
@@ -1,13 +1,14 @@
 const sequelize = require('../config/connection');
-const User = require('../models');
+const User = require('../models/user');
 const userSeeds = require('./user-seeds.json');
 
 const seedUsers = async () => {
-  await sequelize.sync({force:true});
-  await User.bulkCreate(userSeeds, {
+  await sequelize.sync({ force: true });
+  // eslint-disable-next-line no-trailing-spaces
+  
+  const users = await User.bulkCreate(userSeeds, {
     individualHooks: true,
-    returning: true
-
+    returning: true,
   });
 
   process.exit(0);
