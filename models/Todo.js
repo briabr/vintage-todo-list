9240435ab0de6ebac5e1c699678f0d3cf3018d9b
briabr <filalibria@yahoo.fr>
2022-11-17 18:39:24 -0500

    added todo model

---
+++ models/Todo.js
@@ -0,0 +1,41 @@
+const {Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Todo extends Model {};
+
+Todo.init(
+  {
+    id: {
+      type:DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title:{
+      type:DataTypes.STRING,
+      allowNull: false
+    },
+    complete: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        Model:'user',
+        Key:'id',
+        onDelete: 'cascade'
+      }
+    }
+  },
+  {
+    sequelize,
+    modelName:'todo',
+    freezeTableName: true,
+    timestamps: true,
+    underscored: true,
+  }
+
+);
+
+module.exports = Todo;
--- server.js
+++ server.js
@@ -1,12 +1,31 @@
 const express = require('express');
 const sequelize = require('./config/connection');
+const session = require('express-session');
 const app = express();
+const routes = require('./routes');
 //make the port variable dynamic
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+// app.get('/', (req, res) => {
+//   res.send('Hello World!');
+// });
+
+app.use(routes);
+
+const sess = {
+  secret: process.env.SESSION_SECRET,
+  cookies: {},
+  resaved: false,
+  savedUinitialize:true
+
+};
+
+app.use(session(sess));
+
+app.use(express.json());
+app.use(express.urlencoded({extended : true}));
+
+
 
 
 sequelize.sync({ force: false }).then(() => {
